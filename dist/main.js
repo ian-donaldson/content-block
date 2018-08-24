7ea3bca7bb4d30375bf033e930900d563c4e7139
ian-donaldson <31359774+ian-donaldson@users.noreply.github.com>
2018-08-24 12:11:48 +1000

    made same changes to html to centre map frame
    this time in the dist folder- not just src folder

--- dist/main.js
+++ dist/main.js
@@ -116,7 +116,7 @@ function paintMap() {
 	var url = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
 		address.split(' ').join('+') + '&size=' + width + 'x' + height + '&zoom=' + zoom +
 		'&markers=' + address.split(' ').join('+') + '&key=' + mapsKey;
-	sdk.setContent('<a href="' + link + '"><img src="' + url + '" /></a>');
+	sdk.setContent('<div style="text-align:center"><a href="' + link + '"><img src="' + url + '" /></a></div>');
 	sdk.setData({
 		address: address,
 		width: width,
